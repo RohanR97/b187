635d5062c7341bbf8bbeb98c61f98ec47483b951
RohanR97 <rohanrangari123@gmail.com>
2025-04-17 16:02:16 +0530

    Update Data.java
    Updated

--- GiProject/src/com/Data.java
+++ GiProject/src/com/Data.java
@@ -4,5 +4,7 @@ public class Data {
 public static void main(String[] args) {
 	System.out.println("Hello Git.");
 	System.out.println("Hello Again.");
+
+	System.out.println("Hello from git.");
 }
 }
